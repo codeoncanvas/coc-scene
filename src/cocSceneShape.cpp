4c651c10fdcae7d35350a9ae8c6bc6f81e6ed340
lukasz karluk <julapy@googlemail.com>
2016-07-06 15:27:56 +1000

    cocSceneShape

---
+++ src/cocSceneShape.cpp
@@ -0,0 +1,30 @@
+/**
+ *
+ *      ┌─┐╔═╗┌┬┐┌─┐
+ *      │  ║ ║ ││├┤
+ *      └─┘╚═╝─┴┘└─┘
+ *   ┌─┐┌─┐╔╗╔┬  ┬┌─┐┌─┐
+ *   │  ├─┤║║║└┐┌┘├─┤└─┐
+ *   └─┘┴ ┴╝╚╝ └┘ ┴ ┴└─┘
+ *
+ * Copyright (c) 2016 Code on Canvas Pty Ltd, http://CodeOnCanvas.cc
+ *
+ **/
+
+#include "cocSceneShape.h"
+
+namespace coc {
+
+namespace scene {
+
+Shape::Shape() : coc::scene::Object() {
+    //
+}
+
+Shape::~Shape() {
+    //
+}
+
+};
+
+};
---
+++ src/cocSceneShape.h
@@ -0,0 +1,30 @@
+/**
+ *
+ *      ┌─┐╔═╗┌┬┐┌─┐
+ *      │  ║ ║ ││├┤
+ *      └─┘╚═╝─┴┘└─┘
+ *   ┌─┐┌─┐╔╗╔┬  ┬┌─┐┌─┐
+ *   │  ├─┤║║║└┐┌┘├─┤└─┐
+ *   └─┘┴ ┴╝╚╝ └┘ ┴ ┴└─┘
+ *
+ * Copyright (c) 2016 Code on Canvas Pty Ltd, http://CodeOnCanvas.cc
+ *
+ **/
+
+#pragma once
+
+#include "cocSceneObject.h"
+
+namespace coc {
+namespace scene {
+
+class Shape : public coc::scene::Object {
+
+public:
+
+    Shape();
+    ~Shape();
+    
+};
+};
+};
