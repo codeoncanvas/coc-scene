3ae70d3d12bd5e8e3e5cb8e9880788421fab5983
lukasz karluk <julapy@googlemail.com>
2016-07-08 23:37:26 +1000

    SceneCustomObject only header

--- ci-samples/SceneSample/src/SceneCustomObject.h
+++ ci-samples/SceneSample/src/SceneCustomObject.h
@@ -2,14 +2,19 @@
 #pragma once
 
 #include "cocSceneObject.h"
+#include "cinder/gl/gl.h"
 
 class SceneCustomObject : public coc::scene::Object {
 
 public:
 
-    SceneCustomObject(std::string objID="");
-    ~SceneCustomObject();
+    SceneCustomObject(std::string objID="") : coc::scene::Object(objID) {
+        //
+    }
     
-    void draw() const override;
+    void draw() const override {
+        ci::gl::ScopedColor color(ci::Color(0.0, 0.0, 1.0));
+        ci::gl::drawSolidCircle(glm::vec2(0, 0), 5);
+    }
 
 };
