9b6bcc4d111c0acd870c663f48bc307ea247dea0
lukasz karluk <julapy@googlemail.com>
2016-07-08 13:09:36 +1000

    loader svg

---
+++ src/loader/cocSceneLoaderSVG.cpp
@@ -0,0 +1,28 @@
+/**
+ *
+ *      ┌─┐╔═╗┌┬┐┌─┐
+ *      │  ║ ║ ││├┤
+ *      └─┘╚═╝─┴┘└─┘
+ *   ┌─┐┌─┐╔╗╔┬  ┬┌─┐┌─┐
+ *   │  ├─┤║║║└┐┌┘├─┤└─┐
+ *   └─┘┴ ┴╝╚╝ └┘ ┴ ┴└─┘
+ *
+ * Copyright (c) 2016 Code on Canvas Pty Ltd, http://CodeOnCanvas.cc
+ *
+ **/
+
+#include "cocSceneLoaderSVG.h"
+
+namespace coc {
+namespace scene {
+
+LoaderSVG::LoaderSVG() {
+    //
+}
+
+LoaderSVG::~LoaderSVG() {
+    //
+}
+
+};
+};
---
+++ src/loader/cocSceneLoaderSVG.h
@@ -0,0 +1,34 @@
+/**
+ *
+ *      ┌─┐╔═╗┌┬┐┌─┐
+ *      │  ║ ║ ││├┤
+ *      └─┘╚═╝─┴┘└─┘
+ *   ┌─┐┌─┐╔╗╔┬  ┬┌─┐┌─┐
+ *   │  ├─┤║║║└┐┌┘├─┤└─┐
+ *   └─┘┴ ┴╝╚╝ └┘ ┴ ┴└─┘
+ *
+ * Copyright (c) 2016 Code on Canvas Pty Ltd, http://CodeOnCanvas.cc
+ *
+ **/
+
+#pragma once
+
+namespace coc {
+namespace scene {
+
+class LoaderSVG {
+
+public:
+
+    LoaderSVG();
+    ~LoaderSVG();
+    
+    //
+    
+protected:
+    
+    //
+    
+};
+};
+};
